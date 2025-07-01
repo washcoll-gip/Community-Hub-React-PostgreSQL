17ca7d7f4a61a3026230c0c383ab566a91c75793
R4MIRO13 <ramiro.gamboam@pucp.edu.pe>
2025-07-01 10:43:44 -0400

    Third Deployment Attempt

--- postgis-api/index.js
+++ postgis-api/index.js
@@ -11,7 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://es-community-hub.netlify.app/"
+}));
 app.use(express.json());
 
 dotenv.config();
